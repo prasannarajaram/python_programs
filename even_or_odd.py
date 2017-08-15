ea25bdf70d5d3a28ffbacdeac45744e600688ae4
prasannarajaram <prasannarajaram@gmail.com>
2017-08-15 09:22:15 +0530

    updated extras

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,9 +1,24 @@
 # Program to find out if the given number is even or not!
+# Ask the user for a number. Depending on whether the number is even or odd, print out an
+# appropriate message to the user. Hint: how does an even / odd number react differently when divided
+# by 2?
+# 
+# Extras:
+# 
+# 1 If the number is a multiple of 4, print out a different message.
+# 2 Ask the user for two numbers: one number to check (call it num) and one number to divide by
+#  (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate
+#  message.
 
 x = int(input('Enter an integer: '))
-if x%2 == 0:
-    print('{0} is even'.format(x))
+if x%4 == 0:
+    print("%d is even and divisible by 4"%x)
+elif x%2 == 0:
+    print('%d is even'%x)
 else:
-    print('{0} is odd'.format(x))
+    print('%d is odd'%x)
+
+# Extras coding
+# Number divisibility by 4
     
         
