6486818f0605e26a5a16c0a40b8370636f0d9668
prasannarajaram <prasannarajaram@gmail.com>
2017-08-26 18:09:16 +0530

    reviewed

--- list_overlap.py
+++ list_overlap.py
@@ -18,10 +18,12 @@
 #    out at this point - we'll get to it soon)
 
 a = [1, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# a = [1, 1, 1, 2]
 b = [1, 2, 3, 4, 5, 6, 7,8, 9, 10, 11, 12, 13]
 c = [ ]
 d = [ ] 
 i = 0
+
 while i<= (len(a)-1):
   if a[i] in b:
     c.append(a[i])
